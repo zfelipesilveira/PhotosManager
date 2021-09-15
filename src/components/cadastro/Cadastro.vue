92af0e47d81b1666f2cc3c7b342bb415ecfaec0a
zfelipesilveira <zfelipesilveira@gmail.com>
2021-09-15 15:35:29 -0300

    colocando o vee validate em portugues

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -9,12 +9,16 @@
     <form @submit.prevent="grava()">
       <div class="controle">
         <label for="titulo">TÍTULO</label>
-        <input v-model.lazy="foto.titulo" id="titulo" autocomplete="off">
+        <input name="titulo" v-model="foto.titulo" id="titulo" autocomplete="off" 
+        v-validate data-vv-rules="required|min:3|max:30" data-vv-as="título">
+         <span class="erro" v-show="errors.has('titulo')">{{ errors.first('titulo') }}</span>
       </div>
 
       <div class="controle">
         <label for="url">URL</label>
-        <input v-model.lazy="foto.url" id="url" autocomplete="off">
+        <input name="url" v-model="foto.url" id="url" autocomplete="off"
+        v-validate data-vv-rules="required">
+        <span class="erro" v-show="errors.has('url')">{{ errors.first('url') }}</span>
         <imagem-responsiva v-show="foto.url" :url="foto.url" :titulo="foto.titulo"/>
       </div>
 
@@ -60,14 +64,20 @@ export default {
 
     grava() {
 
-      this.service
-        .cadastra(this.foto)
-        .then(() => {
-          if(this.id) this.$router.push({ name: 'home'});
-          this.foto = new Foto()
-        }, 
-        err => console.log(err));
+        this.$validator
+          .validateAll()
+          .then(success => {
+            if(success) {
 
+              this.service
+                .cadastra(this.foto)
+                .then(() => {
+                  if(this.id) this.$router.push({ name: 'home'});
+                  this.foto = new Foto()
+                }, 
+                err => console.log(err));
+            }
+        });
     }
   }, 
 
@@ -107,6 +117,10 @@ export default {
 
   .centralizado {
     text-align: center;
+  }
+
+  .erro {
+    color: red;
   }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -5,9 +5,23 @@ import App from './App.vue'
 import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
 
+
 import { routes } from './routes';
 
 import './directives/Transform';
+
+// importando o VeeValidate 
+import VeeValidate from 'vee-validate';
+import msg from './pt_BR';
+
+Vue.use(VeeValidate, {
+  locale: 'pt_BR',
+  dictionary: {
+    pt_BR: {
+      messages: msg
+    }
+  }
+});
 
 Vue.use(VueRouter);
 
