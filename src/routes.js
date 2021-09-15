08b102d7d15f58db23d1bf67d439d3ed3e7c04ee
zfelipesilveira <zfelipesilveira@gmail.com>
2021-09-15 16:50:58 -0300

    projeto buildado e pronto

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,15 @@
-// alurapic/src/routes.js
+
 
 import Home from './components/home/Home.vue';
-import Cadastro from './components/cadastro/Cadastro.vue';
+
+const Cadastro = () => System.import('./components/cadastro/Cadastro.vue');
+
 
 export const routes = [
 
     { path: '', name: 'home', component: Home, titulo: 'Home', menu: true },
     { path: '/cadastro', name:'cadastro', component: Cadastro, titulo: 'Cadastro', menu: true },
-    { path: '/cadastro/:id', name:'altera', component: Cadastro, titulo: 'Cadastro', menu: false },    
+    { path: '/cadastro/:id', name:'altera', component: Cadastro, titulo: 'Cadastro', menu: false },
     { path: '*', component: Home, menu: false }
 
 ];
