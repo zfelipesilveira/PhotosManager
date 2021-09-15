3d76e5a5f0be31a8c583e3a3ab6d2bfdbde53cdd
zfelipesilveira <zfelipesilveira@gmail.com>
2021-09-15 01:16:26 -0300

    criando o home.vue e limpando o app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,70 +1,14 @@
 <template>
   <div class="corpo">
-
-    <h1 class="centralizado">{{ titulo }}</h1>
-
-    <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="filtre pelo título da foto">
-
-    <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
-        <meu-painel :titulo="foto.titulo">
-          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
-        </meu-painel>
-      </li>
-    </ul>
-
+    <!-- é aqui que encontrará os componentes de página que queremos visualizar -->
   </div>
 </template>
 
 <script>
-
-import Painel from './components/shared/painel/Painel.vue'
-
-export default {
-
-  components: {
-
-    'meu-painel': Painel
-  },
-
-  data () {
-    return {
-      titulo: 'Alurapic', 
-
-      fotos: [],
-
-      filtro: ''
-    }
-  },
-
-  computed: {
-
-    fotosComFiltro() {
-
-      if (this.filtro) {
-        let exp = new RegExp(this.filtro.trim(), 'i');
-        return this.fotos.filter(foto => exp.test(foto.titulo));
-      } else {
-        return this.fotos;
-      }
-
-    }
-  },
-
-  created() {
-
-    this.$http
-      .get('http://localhost:3000/v1/fotos')
-      .then(res => res.json())
-      .then(fotos => this.fotos = fotos, err => console.log(err));
-  }
-}
 </script>
-<style>
 
-  .centralizado {
-    text-align: center;
-  }
+
+<style>
 
   .corpo {
     font-family: Helvetica, sans-serif;
@@ -72,20 +16,4 @@ export default {
     width: 96%;
   }
 
-  .lista-fotos {
-    list-style: none;
-  }
-
-  .lista-fotos .lista-fotos-item {
-    display: inline-block;
-  }
-
-  .imagem-responsiva {
-    width: 100%;
-  }
-
-  .filtro {
-    display: block;
-    width: 100%;
-  }
 </style>
